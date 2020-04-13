8b96fa61d00d827973656d48a48fe482d1f1f9d0
medo ismail <45217544+medoismail@users.noreply.github.com>
2020-04-13 09:43:51 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1644,3 +1644,12 @@ $(window).resize(function (event) {
         });
         return error;
     }
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-163438256-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-163438256-1');
+</script>
